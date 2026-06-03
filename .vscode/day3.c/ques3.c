203021a7127b262c09f835eb19634cb6d71a704b
anushkasankhwar0812-gif <anushkasankhwar0812@gmail.com>
2026-06-03 21:26:15 +0530

    day3

---
+++ .vscode/day3.c/ques3.c
@@ -0,0 +1,25 @@
+//Write a program to Find LCM of two numbers.
+#include <stdio.h>
+
+int main() {
+    int a, b, x, y, temp, gcd, lcm;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    x = a;
+    y = b;
+
+    while (y != 0) {
+        temp = y;
+        y = x % y;
+        x = temp;
+    }
+
+    gcd = x;
+    lcm = (a * b) / gcd;
+
+    printf("LCM = %d", lcm);
+
+    return 0;
+}
